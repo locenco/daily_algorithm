41eea3aaf00c4f9acdf94fcc479f641a98a17b68
张冲 <zhangchong1206@gmail.com>
2021-01-13 09:48:55 +0800

    剑指66、67

---
+++ data_structure/src/main/java/top/macondo/aigorithm/leetcode/JZ01.java
@@ -0,0 +1,34 @@
+package top.macondo.aigorithm.leetcode;
+
+/**
+ * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * @author: zhangchong
+ * @Date: 2020/9/22 10:46
+ **/
+public class JZ01 {
+	public boolean Find(int target, int[][] array) {
+		if(array.length == 0 || array[0].length == 0){
+			return false;
+		}
+
+
+		// 1. 边界和起点（右上角，左侧比当前点小，下方比当前点大，好处是不用考虑右侧的值）
+		int colLength = array[0].length;
+		int rowLength = array.length;
+		int colIndex = colLength -1;
+		int rowIndex = 0;
+		// 2. target大，下移；target小，左移
+		while (colIndex >= 0 && rowIndex <= rowLength - 1) {
+			if(target == array[rowIndex][colIndex]){
+				return true;
+			} else if (target > array[rowIndex][colIndex]) {
+				rowIndex++;
+			} else if (target < array[rowIndex][colIndex]) {
+				colIndex--;
+			}
+		}
+		return false;
+
+	}
+}
---
+++ data_structure/src/main/java/top/macondo/aigorithm/leetcode/JZ66.java
@@ -0,0 +1,34 @@
+package top.macondo.aigorithm.leetcode;
+
+/**
+ * 机器人的运动范围
+ * 地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
+ * @author: zhangchong
+ * @Date: 2020/9/21 14:16
+ **/
+public class JZ66 {
+	public int movingCount(int threshold, int rows, int cols){
+		boolean[][] visited = new boolean[rows][cols];
+		return movingCountHelper(threshold, rows, cols, 0, 0, visited);
+	}
+
+	public int movingCountHelper(int threshold, int rows, int cols, int curRow, int curCol, boolean[][] visited) {
+		if ( curRow < 0 || curCol < 0|| curRow >= rows || curCol >= cols || visited[curRow][curCol] || (bitSum(curRow) + bitSum(curCol) > threshold)) {
+			return 0;
+		}
+		visited[curRow][curCol] = true;
+		return movingCountHelper(threshold, rows, cols, curRow - 1, curCol, visited) +
+						movingCountHelper(threshold, rows, cols, curRow, curCol - 1, visited) +
+						movingCountHelper(threshold, rows, cols, curRow, curCol + 1, visited) +
+						movingCountHelper(threshold, rows, cols, curRow+ 1, curCol , visited) + 1;
+	}
+
+	public int bitSum(int number) {
+		int sum = 0;
+		while (number != 0) {
+			sum += number % 10;
+			number /= 10;
+		}
+		return sum;
+	}
+}
